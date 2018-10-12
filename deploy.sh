fcd308e7fbf0be43deb04e0886a33d9c010f40b3
Hareesh Pattipati <phareesh@gmail.com>
2018-10-12 14:32:29 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t phareesh76/multi-client:latest -t phareesh76/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t phareesh76/multi-server:latest -t phareesh76/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t phareesh76/multi-worker:latest -t phareesh76/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push phareesh76/multi-client:latest
+docker push phareesh76/multi-client:$SHA
+docker push phareesh76/multi-server:latest
+docker push phareesh76/multi-server:$SHA
+docker push phareesh76/multi-worker:latest
+docker push phareesh76/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=phareesh76/multi-server:$SHA
+kubectl set image deployments/client-deployment server=phareesh76/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=phareesh76/multi-worker:$SHA
