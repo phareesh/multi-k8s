5b0cd9bda382930ef740fe8c446d9317261cae0d
Hareesh Pattipati <phareesh@gmail.com>
2018-10-12 14:47:41 -0700

    Updated travis.yml file

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ docker push phareesh76/multi-worker:latest
 docker push phareesh76/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=phareesh76/multi-server:$SHA
-kubectl set image deployments/client-deployment server=phareesh76/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=phareesh76/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=phareesh76/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=phareesh76/multi-worker:$SHA
